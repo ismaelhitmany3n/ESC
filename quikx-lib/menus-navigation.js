5abff8af0a602a7650c729ef705734000e7c280b
ismaelhitmany3n <ismaelhitmany3n@gmail.com>
2019-11-14 21:25:35 -0800

    Add files via upload

---
+++ quikx-lib/menus-navigation.js
@@ -0,0 +1,24 @@
+/*=============================
+===========VARIABLES============
+==============================*/
+
+        window.onscroll=function(){stickynav()};
+
+        var navbar=document.getElementById("navbar");
+        var sticky=navbar.offsetTop;
+
+/*=============================
+===========FUNCTIONS===========
+==============================*/
+
+        
+        function stickynav(){
+       
+            if (window.pageYOffset >= sticky ){
+                //Will ADD/TRIGGER CLASS STICKY to ID NAVBAR
+                navbar.classList.add("sticky");
+                }
+            else{
+                navbar.classList.remove("sticky");
+                }
+        }
